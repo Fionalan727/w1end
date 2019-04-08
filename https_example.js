12096dd91eddb5ccfeca7d8f5679f5bc8a0bac34
Yufei Lan <lanyufei518@gmail.com>
2019-04-08 00:13:15 +0000

    first draft maded

---
+++ https_example.js
@@ -0,0 +1,15 @@
+var https = require('https');
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+//called by https when the request is made
+var callback = function () {
+  console.log('In response handler callback!');
+}
+
+console.log('I am about to make the requests!');
+
+https.request(options, callback).end();
+console.log("I've made the request!");
